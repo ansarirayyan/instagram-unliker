7c73134c58ab1348b6833ea3c1d5a3eeea194252
Rayyan Ansari <ansarirayyan@gmail.com>
2021-05-31 21:31:59 -0700

    I did a thing

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-/* const readline = require("readline");
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-}); */
 const prompts = require('prompts');
 require('dotenv').config()
 const { IgApiClient } = require('./node_modules/instagram-private-api/dist');
@@ -16,16 +11,10 @@ ig.state.generateDevice(process.env.IG_USERNAME);
 // Optionally you can setup proxy url; if you know anyhing about me, I don't like doing anything optional lol
 // ig.state.proxyUrl = process.env.IG_PROXY;
 (async () => {
-  // Execute all requests prior to authorization in the real Android application
-  // Not required but recommended
-//  await ig.simulate.preLoginFlow();
-//console.log("might be an issue here?");
-	//there issue right below habibi; makes sense
-// console.log("here is supposed to be the username:" + process.env.IG_USERNAME); // aha, so tha y nothing there w/o xtra words
-	// this issue has been resolved Alhamdulillah
   const loggedInUser = await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
-	// so this above line actually does indeed return an ... actually idk it gives back 400 bad req
-//console.log("or maybe here");
+// oes that preLoginFlow stuff make it less likely to gte banned
+	// idk i think it said somewher eit was the same thign as something else; oh well
+	// yeah right below
   // The same as preLoginFlow(); wait, what is? the const loggedInUser stuff?
 	// seems like it
 // currentUserPerson = ig.key.currentUser(); //idk what im doing her
@@ -38,37 +27,17 @@ ig.state.generateDevice(process.env.IG_USERNAME);
   //const myPostsFirstPage = await userFeed.items(); // OH THIS VAR NAME LOL THIS IS FOR MY POSTS MY OWN LOL AND IS UNDEFINED
 	console.log("can this be printed?"+loggedInUser.pk);
 
-  const discoverFeed = ig.feed.discover(loggedInUser.pk);
+//  const discoverFeed = ig.feed.discover(loggedInUser.pk);
+  // i don't think these are actual osts but people recommended to you lol
+	// I could add a whole menu option thing but maybe later lol
 
+const discoverFeed = ig.feed.liked(loggedInUser.pk);
+	// Alhamdulillah I think I did it time to push lol
 
 	const items = await discoverFeed.items();
   console.log(items); // Here you can reach items. It's array.
 
-// ok so that diary entyr (lol) I wrote below was befor eme writing thisadsadsad
-	//
-	//
-	/* rl.question("Why are you rinning?? ", function(name) {
-    rl.question("Where do you live ? ", function(country) {
-        console.log(`${name}, is a citizen of ${country}`);
-        rl.close();
-    });
-}); yeha idk*/
 
-/*	prompt.start();
-
-prompt.get(['theUserMyGuyLookinFo', 'email'], function (err, result) {
-    if (err) { return onErr(err); }
-    console.log('Command-line input received:');
-    console.log('  Username: ' + result.theUserMyGuyLookinFo);
-    console.log('  Why are you running? Cuz email??: ' + result.email);
-});
-
-function onErr(err) {
-    console.log(err);
-    return 1;
- } */
-
-//(async () => {
   const response = await prompts({
     type: 'text',
     name: 'desiredUsernameToSearchFor',
@@ -77,41 +46,31 @@ function onErr(err) {
 
   console.log(response.desiredUsernameToSerarchFor); // => { value: 24 } from ex code
 	// should name be capitalized (first leter) in camel case?
-// })();
 
 	userInputtedString = response.desiredUsernameToSearchFor;
 
-  //userInputtedString = "kooltool123" // so apparently this is one of my homie's homies
-	// a homie of my homie is my homie and my homie's In sha Allahs become Al Hamdu Lillahs
-	// was thinking of doing this whole angular web interface at firs but idk too much 
-	// work and idk if it goes against me and my guy raja weise's agreement this kind 
-	// of already goes against it but I don't think he would mind anyways found this
-	// thing called readline on stack overflow what a long comment lemme add some
-	// newlines up there or left there I guess idk man
-	// something nice is that u can use mouse cursor thing weeeeee
   i = 0
   while (i < items.length) {
-	  // this should be only 4 spaces but whatever
 	  // remwmber, items.length is always one more than max index value of array/ls
 	  // i forget what are the differences between array and list again lol
-	  
-	  if (items[i].user.username.includes(userInputtedString)) {
-		  console.log("Is this what you asked for, mate? " + items[i].user.username);
+	 
+          username = items[i].user.username;
+
+	  if (username.includes(userInputtedString)) {
+		  console.log("Is this what you asked for, mate? " + username);
 		  // always gotta type in items[0].user.username; aybe shud make some var
 		  // idk if items[0].user.username.includes(userInputtedString) will work or not
 		  // maybe it will, but maybe sometime sit won't in pure JS on web browsers so that y they always make us reassign vars or something idk
 
-		  console.log("Really? This is what you requested: " + userInputtedString);
-		  // prolly a good idea to put this here
-		  // hald-screen with ... whatever
+		 // console.log("Really? This is what you requested: " + userInputtedString);
 	  } else {
-		console.log("What's up doc?");
+//		console.log("What's up doc?");
+		console.log("This one didn't make the cut: " + username );
 	  }
 
 	  i = i + 1;
   }
   
 
-//	console.log("LOOK HERE => " + items[0].user.username); // WEEEEEEEEEEEEEEEEEEEEEEE ALHAMDULLILLAH IMMA PUSH TO A DIFF BRANCH MAYBE IDK
 
 })();
