e49a3ca9545f9521f55f933a16973b0ce071ae1a
Rayyan Ansari <ansarirayyan@gmail.com>
2021-07-02 14:52:56 -0700

    OMG IM SO DUMB I WAS TRYING TO LIKE STUFF THAT WAS ALREADY LIKED

--- index.js
+++ index.js
@@ -1,31 +1,19 @@
 const prompts = require('prompts');
 require('dotenv').config()
-const { IgApiClient } = require('./node_modules/instagram-private-api/dist');
+const { IgApiClient } = require('instagram-private-api');
 const { sample } = require('lodash');
 const ig = new IgApiClient();
-// console.log("selaam habibi is there an ishu here?"); no there is not
 // You must generate device id's before login.
 // Id's generated based on seed
 // So if you pass the same value as first argument - the same id's are generated every time
 ig.state.generateDevice(process.env.IG_USERNAME);
 // Optionally you can setup proxy url; if you know anyhing about me, I don't like doing anything optional lol
-// ig.state.proxyUrl = process.env.IG_PROXY;
 (async () => {
   const loggedInUser = await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
-// oes that preLoginFlow stuff make it less likely to gte banned
-	// idk i think it said somewher eit was the same thign as something else; oh well
-	// yeah right below
-  // The same as preLoginFlow(); wait, what is? the const loggedInUser stuff?
-	// seems like it
-// currentUserPerson = ig.key.currentUser(); //idk what im doing her
-//	console.log("this is the current user:" + currentUserPerson);
-  // Optionally wrap it to process.nextTick so we dont need to wait ending of this bunch of requests
-  // process.nextTick(async () => await ig.simulate.postLoginFlow());
-	// don't think this is needed according to account-followers.feed.example.ts example
+  process.nextTick(async () => await ig.simulate.postLoginFlow());
   // Create UserFeed instance to get loggedInUser's posts
   //const userFeed = ig.feed.user(loggedInUser.pk);
   //const myPostsFirstPage = await userFeed.items(); // OH THIS VAR NAME LOL THIS IS FOR MY POSTS MY OWN LOL AND IS UNDEFINED
-	console.log("can this be printed?"+loggedInUser.pk);
 
 //  const discoverFeed = ig.feed.discover(loggedInUser.pk);
   // i don't think these are actual osts but people recommended to you lol
@@ -35,7 +23,7 @@ const likedFeed = ig.feed.liked(loggedInUser.pk);
 	// Alhamdulillah I think I did it time to push lol
 
 	const items = await likedFeed.items();
-  console.log(items); // Here you can reach items. It's array.
+  // console.log(items); // Here you can reach items. It's array.
 
 
   const response = await prompts({
@@ -44,44 +32,35 @@ const likedFeed = ig.feed.liked(loggedInUser.pk);
     message: 'Which dude you wanna filter for?: ',
   });
 
-  console.log(response.desiredUsernameToSerarchFor); // => { value: 24 } from ex code
-	// should name be capitalized (first leter) in camel case?
+  console.log(response.desiredUsernameToSerarchFor);
 
 	userInputtedString = response.desiredUsernameToSearchFor;
 
   i = 0
   while (i < items.length) {
-	  // remwmber, items.length is always one more than max index value of array/ls
-	  // i forget what are the differences between array and list again lol
-	 
+
           username = items[i].user.username;
 	  postId = items[i].id
 
 	  if (username.includes(userInputtedString)) {
 		  console.log("Is this what you asked for, mate? " + username);
-		  // always gotta type in items[0].user.username; aybe shud make some var
-		  // idk if items[0].user.username.includes(userInputtedString) will work or not
-		  // maybe it will, but maybe sometime sit won't in pure JS on web browsers so that y they always make us reassign vars or something idk
 
-		 // console.log("Really? This is what you requested: " + userInputtedString);
-		 console.log(":::::::LOOK HERE: " + postId);
+		  console.log(":::::::LOOK HERE: " + postId);
 		  //ig.media.unlike(postId);
-		  await ig.media.like({
-    mediaId: postId,
-    moduleInfo: {
-      module_name: 'profile',
-      user_id: loggedInUser.pk,
-      username: loggedInUser.username,
-    },
-  });
+		  const likeResult = await ig.media.unlike({
+        mediaId: postId,
+        moduleInfo: {
+          module_name: 'profile',
+          user_id: loggedInUser.pk,
+          username: loggedInUser.username,
+        },
+        d: 1,
+      });
+      console.log(likeResult);
 	  } else {
-//		console.log("What's up doc?");
-		console.log("This one didn't make the cut: " + username );
+		  console.log("This one didn't make the cut: " + username );
 	  }
 
 	  i = i + 1;
   }
-  
-
-
 })();
